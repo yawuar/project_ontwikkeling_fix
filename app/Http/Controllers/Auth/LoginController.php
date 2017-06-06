118ee5d75c0366b9b43f736a102728ae5a8c9a5d
Adriaan Marain <adriaanmarain300@gmail.com>
2017-06-07 00:45:09 +0200

    redirect & deleted register

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/cms';
 
     /**
      * Create a new controller instance.
