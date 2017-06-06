0aa6e18e5d7653c5ee5dbd0805662324c7dd1962
Adriaan Marain <adriaanmarain300@gmail.com>
2017-06-07 00:47:16 +0200

    logout redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,6 +26,7 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = '/cms';
+    protected $redirectAfterLogout = '/cms';
 
     /**
      * Create a new controller instance.
