fbfbbedf0627a93dcad968107cb282733f8074f7
Yawuar Serna Delgado <yawuarsernadelgado@hotmail.be>
2017-06-08 12:09:49 +0200

    add google play store btn

--- resources/views/pages/city.blade.php
+++ resources/views/pages/city.blade.php
@@ -26,6 +26,7 @@
 
   <section id="game">
     <div class="container">
+      <a href="#" class="play"></a>
       <div class="scoreBoard">
         <div class="head">Scoreboard</div>
         <div class="item">
