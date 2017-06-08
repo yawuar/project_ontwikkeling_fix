e9a31ed2dfd71f9aca7905bf800ecff6e7367dc0
Yawuar Serna Delgado <yawuarsernadelgado@hotmail.be>
2017-06-08 10:00:52 +0200

    fix responsive events page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
-<!-- @extends('layouts.app') -->
+@extends('layouts.cms')
 
-<!-- @section('content') -->
+@section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -65,4 +65,4 @@
         </div>
     </div>
 </div>
-<!-- @endsection -->
+@endsection
